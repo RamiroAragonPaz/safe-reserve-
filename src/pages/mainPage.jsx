c61eb7ab3d1202fe3c47ffcd8ed3469c6a0ff267
Ramiro Aragon Paz <ramiroaragonpaz@gmail.com>
2023-10-08 11:58:31 -0300

    mock

--- src/pages/mainPage.jsx
+++ src/pages/mainPage.jsx
@@ -1,14 +1,42 @@
 import React from 'react';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
+import data from '../components/data.json'
 
-const MainPage = () => (
-    <>
-    <div className='button-container'>
-      <ConnectButton />
-    </div>
-    <h1 className="title-index">Safe Reserve - Tu alquiler seguro</h1>
-    <p>Encontrar un lugar seguro para disfrutar tus momentos, nunca fue más seguro y fácil gracias a la Web3</p>
-  </>
-);
+const MainPage = () => {
+
+  const housesData = data;
+
+  return(
+      <>
+      <div className='button-container'>
+        <ConnectButton />
+      </div>
+      <h1 className="title-index">Safe Reserve - Tu alquiler seguro</h1>
+      <p>Encontrar un lugar seguro para disfrutar tus momentos, nunca fue más seguro y fácil gracias a la Web3</p>
+      <h3>ESTAS SON TUS PROPIEDADES</h3>
+      <div className='houses-container'>
+      {housesData.map((house, index)=>{
+        return(
+          <div className='card' key={index}>
+            <div className='img-container'>
+              <img className='img-element' src={`./${house.images}`} alt={index}/>
+            </div>
+            <div>
+            <p>Tipo: {house.type}</p>
+            </div>
+            <div>
+              <p>Habitaciones: {house.rooms}</p>
+            </div>
+            <div>
+              <p>Ubicación: {house.location}</p>
+            </div>
+            <div></div>
+          </div>
+        )
+      })}
+      </div>
+    </>
+    )
+};
 
 export default MainPage;
