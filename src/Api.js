80c59aa11028b01afb30acb1e15cb6f027d5b30a
Facundo Medina <facutota@gmail.com>
2023-10-08 13:24:22 -0300

    ca

--- src/Api.js
+++ src/Api.js
@@ -23,7 +23,7 @@ export const registerUser = async (userData) => {
 
 export const loginUser = async (userData) => {
     try {
-      const response = await fetch(`https://safe-reserve-api.onrender.com//v1/user/login`, {
+      const response = await fetch(`https://safe-reserve-api.onrender.com/v1/user/login`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
